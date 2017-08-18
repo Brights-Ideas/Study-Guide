6fd5f6150ef3e9fb3b7b6d8a1a581f9930bce832
Brighty <abright2012@gmail.com>
2017-08-18 12:08:48 +0100

    Add project files.

---
+++ Study Guide/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Study_Guide.MvcApplication" Language="C#" %>
---
+++ Study Guide/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Study_Guide.Startup))]
+namespace Study_Guide
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
